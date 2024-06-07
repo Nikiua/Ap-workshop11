4723209b2f3b1a71633d62d99dee6387a6961a25
Nikiua <nika.feizi84@gmail.com>
2024-06-07 12:21:11 +0330

    scene measurements made

--- src/main/java/org/example/calculator/Calculator.java
+++ src/main/java/org/example/calculator/Calculator.java
@@ -2,8 +2,10 @@ package org.example.calculator;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -20,6 +22,14 @@ public class Calculator extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+        display = new TextField();
+        display.setPrefSize(300, 80);
+        display.setEditable(false);
+
+        GridPane gridPane = new GridPane();
+        gridPane.setPadding(new Insets(10));
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
 
     }
 
